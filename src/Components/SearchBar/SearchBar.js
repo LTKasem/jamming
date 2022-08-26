3e82eae0627b443c326a08407f987545cabdad60
Kasem <101024234+LTKasem@users.noreply.github.com>
2022-08-26 19:53:47 +0100

    Update SearchBar.js
    search on type

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -14,12 +14,12 @@ class SearchBar extends React.Component {
     handleTermChange(e){
      const result = e.target.value
      this.setState({term:result})
+     this.search()
     }
     render() {
         return (
             <div className="SearchBar">
                 <input onChange={this.handleTermChange} placeholder="Enter A Song, Album, or Artist" />
-                <button onClick={this.search}className="SearchButton">SEARCH</button>
             </div>
         );
     }
