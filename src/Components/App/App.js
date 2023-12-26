ceb08bc548743a9c82568da0d3943db31d11dc72
Kasem <101024234+LTKasem@users.noreply.github.com>
2023-12-26 11:21:01 +0000

    Update App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -48,7 +48,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <h1>My<span className="highlight">Spotify</span>List</h1>
         <div className="App">
           <SearchBar onSearch={this.search}/>
           <div className="App-playlist">
